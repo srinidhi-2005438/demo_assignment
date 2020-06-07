bfb25d5fc9184a969afee930bbb8edc3aa110299
srinidhi-2005438 <66458551+srinidhi-2005438@users.noreply.github.com>
2020-06-07 13:48:43 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -4,18 +4,18 @@ int main()
    int a[]={10,25,24,15,15,57,64};
    int i;
    int n=sizeof(a)/sizeof(a[1]);
-   int b[100];
+   //int b[100];
    for(i=n-1;i>=0;i--)
    {
-   	b[i]=a[i];
+   	printf("%d ",a[i]);
    }
-   printf("b[]={");
+   /*printf("b[]={");
    for(i=n-1;i>0;i--)
    {
    printf("%d,",b[i]);
    }
    printf("%d",b[0]);
-   printf("}"); 
+   printf("}");*/ 
  return 0;
    
 }
