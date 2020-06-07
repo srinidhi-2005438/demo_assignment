e752bdcd2252b1237550ee2c3809dd1124a61bfb
srinidhi-2005438 <66458551+srinidhi-2005438@users.noreply.github.com>
2020-06-07 13:42:54 +0530

    Create daysofweek.c

---
+++ daysofweek.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int day;
+	scanf("%d",&day);
+	switch(day)
+	{
+		case 1: printf("Monday");
+		          break;
+		case 2: printf("Tuesday");
+		          break;
+		case 3: printf("Wednesday");
+		          break;
+		case 4: printf("Thursday");
+		          break;
+		case 5: printf("Friday");
+		          break;
+		case 6: printf("Saturday");
+		          break;
+		case 7: printf("Sunday");
+		          break;
+		default: printf("Invalid entry(please choose between 1 to 7)\n");
+	}
+	return 0;
+}
